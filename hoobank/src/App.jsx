b4838f17fe38f498460cb3b55133511ba27005bd
mlukicdesign <mlukicdesign@gmail.com>
2023-09-14 11:40:31 +0800

    Finished Navbar component

--- hoobank/src/App.jsx
+++ hoobank/src/App.jsx
@@ -6,6 +6,7 @@ import {
   Billing,
   CardDeal,
   Button,
+  Business,
   Footer,
   Hero,
   Clients,
@@ -21,26 +22,26 @@ const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-             Navbar
+             <Navbar/>
           </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+         <Footer/>
         </div>
       </div>
 
--- hoobank/src/components/index.js
+++ hoobank/src/components/index.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Billing from "./Billing";
+import Business from "./Business";
 import Button from "./Button";
 import CardDeal from "./CardDeal";
 import Clients from "./Clients";
@@ -14,6 +15,7 @@ import Testimonials from "./Testimonials";
 export {
     Navbar,
     Billing,
+    Business,
     CardDeal,
     Button,
     Footer,
